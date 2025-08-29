8699afd51d3c13ccdc4200c7f896112ef3039e93
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2025-08-29 13:42:13 -0500

    tester

--- test_compiled_matlab.sh
+++ test_compiled_matlab.sh
@@ -4,8 +4,8 @@ export PATH=$(pwd)/matlab/bin:$PATH
 
 run_matlab_entrypoint.sh \
     /home/rogersbp/MATLAB/R2023a \
-	fmriprep1_dir $(pwd)/INPUTS/fmriprep-ert1 \
-	fmriprep2_dir $(pwd)/INPUTS/fmriprep-ert2 \
+	fmriprep1_dir INPUTS/fmriprep-ert1 \
+	fmriprep2_dir INPUTS/fmriprep-ert2 \
 	hpf_sec 300 \
-	psydat_csv $(pwd)/OUTPUTS/ert1.csv \
-	out_dir $(pwd)/OUTPUTS
+	psydat_csv OUTPUTS/ert1.csv \
+	out_dir OUTPUTS
