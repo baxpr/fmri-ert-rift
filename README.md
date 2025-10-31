e178337cd27ecc48260e7871a2c73fcb75ee9d0c
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2025-10-31 11:43:30 -0500

    readme

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-SPM12 analysis for ERT, RIFT fMRI tasks
+# ERT task
+
+## Inputs
+
+- fMRI in atlas space, preprocessed with fmriprep, for the six task runs (ERT_1, ERT_2, RIFT_1, RIFT_2, RIFT_3, RIFT_4)
+
+- CSV format task timing files for the ERT and RIFT sections of the task. These are the output of https://github.com/VUIIS/psychopy-iis
+
+
+## Methods
+
+For the ERT task, these conditions are modeled:
+
+    Valence      Task
+    
+    neutral      LOOK
+    negative     LOOK
+    negative     ACCEPT
+    negative     AVOID
+    negative     DISTRACT
+    negative     REFRAME
+
+For each condition, the image presentation period and the response period are modeled separately.
+
+Each period is modeled as a block convolved with the standard SPM12 hemodynamic response function.
+
+The second trial of every RIFT pair in the four RIFT runs is ignored and not modeled.
+
+The number of trials per run varies, so contrasts are weighted across runs such that each individual trial receives equal weight.
+
+
+## Outputs
+
+Several `spmbatch` files containing the design and analysis configuration.
+
+Standard SPM12 results directory, `spm_ert`
+
+A PDF suitable for quick visual QC review, `first_level_report_ert.pdf`.
+
+
--- src/pipeline_entrypoint.sh
+++ src/pipeline_entrypoint.sh
@@ -13,9 +13,14 @@ export out_dir=/OUTPUTS
 while [[ $# -gt 0 ]]; do
     key="${1}"
     case $key in   
-        --fmriprep1_dir) export fmriprep1_dir="${2}"; shift; shift ;;
-        --fmriprep2_dir) export fmriprep2_dir="${2}"; shift; shift ;;
-        --psydat_csv) export psydat_csv="${2}"; shift; shift ;;
+        --fmriprep_ert1_dir) export fmriprep_ert1_dir="${2}"; shift; shift ;;
+        --fmriprep_ert2_dir) export fmriprep_ert2_dir="${2}"; shift; shift ;;
+        --fmriprep_rift1_dir) export fmriprep_rift1_dir="${2}"; shift; shift ;;
+        --fmriprep_rift2_dir) export fmriprep_rift2_dir="${2}"; shift; shift ;;
+        --fmriprep_rift3_dir) export fmriprep_rift3_dir="${2}"; shift; shift ;;
+        --fmriprep_rift4_dir) export fmriprep_rift4_dir="${2}"; shift; shift ;;
+        --ert_psydat_csv) export ert_psydat_csv="${2}"; shift; shift ;;
+        --rift_psydat_csv) export rift_psydat_csv="${2}"; shift; shift ;;
         --hpf_sec) export hpf_sec="${2}"; shift; shift ;;
         --fwhm_mm) export fwhm_mm="${2}"; shift; shift ;;
         --out_dir) export out_dir="${2}"; shift; shift ;;
@@ -26,9 +31,14 @@ done
 # Run the matlab part of the pipeline in xvfb
 xvfb-run -n $(($$ + 99)) -s '-screen 0 1600x1200x24 -ac +extension GLX' \
     run_spm12.sh ${MATLAB_RUNTIME} function matlab_entrypoint \
-    fmriprep1_dir "${fmriprep1_dir}" \
-    fmriprep2_dir "${fmriprep2_dir}" \
-    psydat_csv "${psydat_csv}" \
+    fmriprep_ert1_dir "${fmriprep_ert1_dir}" \
+    fmriprep_ert2_dir "${fmriprep_ert2_dir}" \
+    fmriprep_rift1_dir "${fmriprep_rift1_dir}" \
+    fmriprep_rift2_dir "${fmriprep_rift2_dir}" \
+    fmriprep_rift3_dir "${fmriprep_rift3_dir}" \
+    fmriprep_rift4_dir "${fmriprep_rift4_dir}" \
+    ert_psydat_csv "${ert_psydat_csv}" \
+    rift_psydat_csv "${rift_psydat_csv}" \
     hpf_sec "${hpf_sec}" \
     fwhm_mm "${fwhm_mm}" \
     out_dir "${out_dir}"
